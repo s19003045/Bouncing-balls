5e610f6c70cf2da9ee487493d83fefef580ea5a7
s19003045 <s19003045@gmail.com>
2019-08-13 20:52:21 +0800

    init

---
+++ main-finished.js
@@ -0,0 +1,210 @@
+
+// 設定畫佈
+let canvas = document.querySelector('canvas')
+let ctx = canvas.getContext('2d')
+let width = canvas.width = window.innerWidth
+let height = canvas.height = window.innerHeight
+
+// 設定球數
+let ballCounts = 0
+let ballCountDisplay = document.querySelector('span')
+// 亂數產生函式
+function random(min, max) {
+  let num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
+}
+
+
+// Shape constructor
+function Shape(x, y, velX, velY, exists) {
+  this.x = x
+  this.y = y
+  this.velX = velX
+  this.velY = velY
+  this.exists = exists
+}
+
+// Ball constructor：inherit Shape
+function Ball(x, y, velX, velY, color, size, exists) {
+  Shape.call(this, x, y, velX, velY, exists)
+  this.color = color
+  this.size = size
+}
+
+// Ball的draw method
+Ball.prototype.draw = function () {
+  ctx.beginPath()
+  ctx.fillStyle = this.color
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+  ctx.fill()
+}
+
+// Ball的update method
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX)
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX)
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY)
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY)
+  }
+
+  this.x += this.velX
+  this.y += this.velY
+}
+
+// 定義邪惡圈：inherit Shape
+function EvilCircle(x, y, velX, velY, color, size, exists) {
+  Shape.call(this, x, y, exists)
+  this.color = color
+  this.size = size
+  this.velX = velX
+  this.velY = velY
+}
+
+// 定義邪惡圈的draw method
+EvilCircle.prototype.draw = function () {
+  ctx.beginPath()
+  ctx.lineWidth = 3
+  ctx.strokeStyle = this.color
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+  ctx.stroke()
+}
+
+// 定義邪惡圈的update method
+EvilCircle.prototype.checkBounds = function () {
+
+  if ((this.x + this.size) >= width) {
+    this.x = width - this.size * 2
+    this.size -= 1
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.x = 0 + this.size * 2
+    this.size -= 1
+  }
+  if ((this.y + this.size) >= height) {
+    this.y = height - this.size * 2
+    this.size -= 1
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.y = 0 + this.size * 2
+    this.size -= 1
+  }
+}
+
+
+
+// 建立balls array，讓所有球的物件儲存於此
+let balls = []
+while (balls.length < 25) {
+  let size = random(10, 20)
+  let ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+    size,
+    true
+  )
+  balls.push(ball)
+  ballCounts += 1
+  ballCountDisplay.textContent = ballCounts
+}
+
+// Ball的collisionDetect method
+Ball.prototype.collisionDetect = function () {
+  for (j = 0; j < balls.length; j++) {
+    // 若此球與另一球的屬性exists皆為true
+    if (this.exists && balls[j].exists) {
+      // 依此球與其他球的距離，來決定是否變色
+      if (!(this === balls[j])) {
+        let dx = this.x - balls[j].x
+        let dy = this.y - balls[j].y
+        let distance = Math.sqrt(dx * dx + dy * dy)
+
+        // 若兩球球心的距離 <= 兩球半徑相加，則兩球的color變色
+        if (distance <= (this.size + balls[j].size)) {
+          this.color = balls[j].color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')'
+        }
+      }
+    }
+
+  }
+}
+
+// EvilCircle 的setControls method
+EvilCircle.prototype.setControls = function () {
+  // listen to keyboard
+  var _this = this;
+  console.log(this)
+  window.onkeydown = function (e) {
+    if (e.keyCode === 37) {
+      _this.x -= _this.velX;
+    } else if (e.keyCode === 39) {
+      _this.x += _this.velX;
+    } else if (e.keyCode === 38) {
+      _this.y -= _this.velY;
+    } else if (e.keyCode === 40) {
+      _this.y += _this.velY;
+    }
+  }
+}
+
+// EvilCircle 的collisionDetect method
+EvilCircle.prototype.collisionDetect = function () {
+
+  let _this = this
+  for (j = 0; j < balls.length; j++) {
+    // 依此球與其他球的距離，來決定是否變色
+    if (balls[j].exists === true) {
+      let dx = this.x - balls[j].x
+      let dy = this.y - balls[j].y
+      let distance = Math.sqrt(dx * dx + dy * dy)
+
+      // 若兩球球心的距離 <= 兩球半徑相加，則兩球的color變色
+      if (distance <= (this.size + balls[j].size)) {
+        balls[j].exists = false
+        _this.size += 1
+        ballCounts -= 1
+        ballCountDisplay.textContent = ballCounts
+      }
+    }
+  }
+}
+
+// ============執行=========================
+// 建立一個evilCircle實例
+let evilCircle = new EvilCircle(150, 150, 15, 15, 'white', 15, true)
+evilCircle.setControls()
+// 設定loop
+function loop() {
+  ctx.fillStyle = 'rgba(0,0,0,0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (let i = 0; i < balls.length; i++) {
+    if (balls[i].exists === true) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect()
+    }
+  }
+  evilCircle.checkBounds()
+  evilCircle.draw()
+  evilCircle.collisionDetect()
+
+  requestAnimationFrame(loop);
+}
+
+// 執行loop
+loop();
